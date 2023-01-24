225f84e333f81c9500fb9786552df3bc3fccf3f0
Jagholin <pavel.belskiy2@gmail.com>
2023-01-24 17:17:54 +0100

    add a comment

--- routes/todos.js
+++ routes/todos.js
@@ -5,7 +5,10 @@ const router = Router();
 
 // this will get all the todos
 router.get("/", getTodos);
-
 router.post("/", postTodo);
+
+// put - to change a single todo
+// delete - to delete a single todo
+// get("/:id") - to get a single todo
 
 export default router;
