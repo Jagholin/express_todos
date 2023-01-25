38a9b9c1980a615df7541e70de0140556db215c9
Jagholin <pavel.belskiy2@gmail.com>
2023-01-25 16:45:14 +0100

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 
 import sql from "./db/db.js";
+import cors from "cors";
 
 import todosRouter from "./routes/todos.js";
 
 const app = express();
+app.use(cors());
 const PORT = process.argv[2] || 3000;
 
 app.use(express.json());
